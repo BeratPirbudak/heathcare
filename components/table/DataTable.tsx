74a5c45a1673a8497efdd3f30ddd48459b260ced
Berat Pirbudak <beratpirbudak05@outlook.com>
2024-09-14 20:51:23 +0100

    I have partially started the admin dashboard. so far i have only done the statistics and nothing else. I am about to start the data table and then i will finish the project once thiss functionality works. I will then add some more functionality

---
+++ components/table/DataTable.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type DataTableTypes = {
+  columns: any;
+  data: any;
+}
+
+const DataTable = ({columns, data}: DataTableTypes) => {
+  return (
+    <div>DataTable</div>
+  )
+}
+
+export default DataTable
--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,8 +1,9 @@
 "use server"
 
-import { ID } from "node-appwrite"
+import { ID, Query } from "node-appwrite"
 import { APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases } from "../appwrite.config"
 import { parseStringify } from "../utils"
+import { Appointment } from "@/types/appwrite.types"
 
 export const createAppointment = async (appointment: CreateAppointmentParams) => {
     try{
@@ -29,6 +30,47 @@ export const getAppointments = async (appointmentId: string) => {
 
         return parseStringify(appointments)
     }catch (error){
+        console.log(error)
+    }
+}
+
+export const getRecentAppointmentList = async () => {
+    try{
+        const appointments = await databases.listDocuments(
+            DATABASE_ID!,
+            APPOINTMENT_COLLECTION_ID!,
+            [Query.orderDesc('$createdAt')]
+        );
+
+        const initialCounts = {
+            scheduled: 0,
+            pending: 0,
+            cancelled: 0
+        }
+
+        const counts = (appointments.documents as Appointment[]).reduce((acc, appointment) => {
+            if(appointment.status === 'scheduled'){
+                acc.scheduled += 1
+            }else if(appointment.status === 'pending'){
+                acc.pending += 1
+            }else if(appointment.status === 'cancelled'){
+                acc.cancelled += 1
+            }
+
+            return acc;
+        }, initialCounts);
+        
+
+        const data ={
+            totalCount: appointments.total,
+            ...counts,
+            documents: appointments.documents
+        }
+
+
+        return parseStringify(data)
+
+    }catch(error){
         console.log(error)
     }
 }
